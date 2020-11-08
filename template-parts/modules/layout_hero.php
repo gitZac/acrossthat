a3e3d414b98937159d9fafcd4e633c28308c8896
Zac <ztaylor555@gmail.com>
2020-11-08 08:53:55 -0500

    Finished merging styles

--- template-parts/modules/layout_hero.php
+++ template-parts/modules/layout_hero.php
@@ -28,14 +28,14 @@ $use_breadcrumbs = get_sub_field('use_breadcrumbs');
         <?php if($use_breadcrumbs == 'yes') : ?>
             <?php get_template_part( 'template-parts/modules/breadcrumbs' ); ?>
         <?php endif; ?>
-        <h1 class="header header--main hero--underline "><?php if(!empty($header) ){ echo $header; } else{ the_title( ); }  ?></h1>
+        <h1 class="header header--main hero--underline"><?php if(!empty($header) ){ echo $header; } else{ the_title( ); }  ?></h1>
         <?php echo $intro; ?>
     </div>
 </section>
 
 <?php } elseif($style == 'two_columns' ) { ?>
 
-<section id="hero" class="hero d-flex align-items-center" style="background:<?php echo $bg_color; ?>; height:<?php echo $height; ?>vh; ">
+<section class="hero hero--two-col d-flex align-items-center" style="background:<?php echo $bg_color; ?>; height:<?php echo $height; ?>vh; ">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
@@ -46,8 +46,8 @@ $use_breadcrumbs = get_sub_field('use_breadcrumbs');
                     <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn--watch" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
                 </div>
             </div>
-            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-            <img src="<?php echo get_template_directory_uri(  ) . '/dist/assets/img/hero-img.png'; ?>" class="img-fluid animated" alt="">
+            <div class="hero__image col-lg-6 order-1 order-lg-2 " data-aos="zoom-in" data-aos-delay="200">
+                <img src="<?php echo get_template_directory_uri(  ) . '/dist/assets/img/hero-img.png'; ?>" class="img-fluid animate--infinite" alt="">
             </div>
         </div>
     </div>
