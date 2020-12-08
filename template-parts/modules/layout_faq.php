151126a1a9d9da01ab791fcb40a9226090077ed4
Zac <ztaylor555@gmail.com>
2020-12-08 14:24:04 -0500

    finished faq

--- template-parts/modules/layout_faq.php
+++ template-parts/modules/layout_faq.php
@@ -1,17 +1,22 @@
-<?php if(have_rows('fp_faq') ) : ?>
+<?php 
+  $section_bg = get_sub_field('section_bg');
+  $section_title = get_sub_field('section_title');
+  $section_description = get_sub_field('section_description');?>
     <section id="faq" class="faq section-bg">
       <div class="container">
-        <div class="section-title">
-          <h2><?php the_field('faq_section_title'); ?></h2>
-        </div>
-        <div class="row  d-flex align-items-stretch">
-            <?php while(have_rows('fp_faq') ) : the_row(); ?>
+        <?php if($section_title) : ?>
+          <div class="section-title">
+              <h2><?php echo $section_title; ?></h2>
+              <p><?php echo $section_description; ?></p>
+          </div>
+      <?php endif; ?>
+        <div class="row d-flex align-items-stretch">
+            <?php if(have_rows('fp_faq') ) : while(have_rows('fp_faq') ) : the_row(); ?>
                 <div class="col-lg-6 faq-item" data-aos="fade-up">
                     <h4><?php the_sub_field('faq_question'); ?></h4>
                     <p><?php the_sub_field('faq_answer'); ?></p>
                 </div>
-            <?php endwhile; ?>
+            <?php endwhile; endif; ?>
         </div>
       </div>
     </section>
-<?php endif; ?>
