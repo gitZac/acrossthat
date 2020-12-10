5497a3671769909206db235e1fd07fd879614852
Zac <ztaylor555@gmail.com>
2020-12-10 13:52:43 -0500

    Light refactoring of process

--- template-parts/modules/layout_faq.php
+++ template-parts/modules/layout_faq.php
@@ -1,22 +1,22 @@
 <?php 
   $section_bg = get_sub_field('section_bg');
   $section_title = get_sub_field('section_title');
-  $section_description = get_sub_field('section_description');?>
-    <section id="faq" class="faq section-bg">
-      <div class="container">
-        <?php if($section_title) : ?>
-          <div class="section-title">
-              <h2><?php echo $section_title; ?></h2>
-              <p><?php echo $section_description; ?></p>
-          </div>
-      <?php endif; ?>
-        <div class="row d-flex align-items-stretch">
-            <?php if(have_rows('fp_faq') ) : while(have_rows('fp_faq') ) : the_row(); ?>
-                <div class="col-lg-6 faq-item" data-aos="fade-up">
-                    <h4><?php the_sub_field('faq_question'); ?></h4>
-                    <p><?php the_sub_field('faq_answer'); ?></p>
-                </div>
-            <?php endwhile; endif; ?>
+  $section_description = get_sub_field('section_description'); ?>
+  <section class="faq section-bg <?php if($section_bg == 'yes') { echo 'section-bg'; } ?>">
+    <div class="container">
+      <?php if($section_title) : ?>
+        <div class="section-title">
+            <h2><?php echo $section_title; ?></h2>
+            <p><?php echo $section_description; ?></p>
         </div>
+    <?php endif; ?>
+      <div class="row d-flex align-items-stretch">
+          <?php if(have_rows('fp_faq') ) : while(have_rows('fp_faq') ) : the_row(); ?>
+              <div class="col-lg-6 faq-item" data-aos="fade-up">
+                  <h4><?php the_sub_field('faq_question'); ?></h4>
+                  <p><?php the_sub_field('faq_answer'); ?></p>
+              </div>
+          <?php endwhile; endif; ?>
       </div>
-    </section>
+    </div>
+  </section>
