9f7c9b94e359e33e58c07019cf94f16a048f72bc
Zac <ztaylor555@gmail.com>
2020-12-28 08:17:02 -0500

    Adding alternate styeles

--- footer.php
+++ footer.php
@@ -4,63 +4,73 @@
  *
  * Contains the closing of the #content div and all content after.
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package AcrossThat
  */
 
 ?>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
 
-<!-- ======= Footer ======= -->
-<footer class="footer" id="footer">
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Join Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="footer-top">
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <div class="footer__logo-container">
-              <a href="index.html"><img src="<?php echo get_template_directory_uri( ) . '/dist/assets/img/logo5-white.png' ?>" alt="" class="img-fluid"></a>
-            </div>
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Arsha</h3>
             <p>
-              <?php the_field('address', 'option'); ?> <br>
-              <strong>Phone: </strong><?php the_field('phone', 'option'); ?><br>
-              <strong>Email: </strong><?php the_field('email', 'option'); ?><br>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
             </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <?php dynamic_sidebar( 'footer-area-1' ); ?>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <?php dynamic_sidebar( 'footer-area-2' ); ?>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
             <div class="social-links mt-3">
               <?php get_template_part('/template-parts/loop/loop-options'); ?>
+
             </div>
-		  </div>
-
-		  <div class="col-lg-2 col-md-6 footer-links">
-		  	<?php dynamic_sidebar( 'footer-area-1' ); ?>
-		  </div>
-
-		  <div class="col-lg-3 col-md-6 footer-links">
-			 <?php dynamic_sidebar( 'footer-area-2' ); ?>
-		  </div>
-
-		  <div class="col-lg-4 col-md-6 footer-links">
-		  	<?php dynamic_sidebar( 'footer-area-3' ); ?>
-		  </div>
-		
+          </div>
 
         </div>
       </div>
     </div>
 
-    <div class="container">
+    <div class="container footer-bottom clearfix">
       <div class="copyright">
-        &copy; Copyright <strong><span>FSTAN</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/ -->
-        Proudly built by <a style="color:#fff;" href="https://acrossthat.com">AcrossThat Studios</a>
-      </div>
+      
     </div>
   </footer><!-- End Footer -->
+
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 </div><!-- #page -->
